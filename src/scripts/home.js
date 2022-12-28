ad4059f2b185ed191f9edb5016c913f71c5c61fd
CauPis <caua@sitecbr.com.br>
2022-12-28 16:38:23 -0300

    Commmit inicial

---
+++ src/scripts/home.js
@@ -0,0 +1,10 @@
+console.log("Here");
+$('.fade').slick({
+    infinite: true,
+    dots: false,
+    speed: 800,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+  });
